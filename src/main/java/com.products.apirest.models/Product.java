200e770f1146b30e80f047f5dc264f5523e58669
Gilvan Henrique <gilvanh.15@gmail.com>
2020-09-16 21:09:58 -0300

    produts model attributes

--- src/main/java/com.products.apirest.models/Product.java
+++ src/main/java/com.products.apirest.models/Product.java
@@ -1,11 +1,23 @@
 package com.products.apirest.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name="TB_PRODUCT")
-public class Product {
+public class Product implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    private BigDecimal quantidade;
+
+    private BigDecimal valor;
 
 }
