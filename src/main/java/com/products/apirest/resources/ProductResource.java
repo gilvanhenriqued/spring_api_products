07696e727a54fe2f59f32c500ea1a6a7f2ad4264
Gilvan Henrique <gilvanh.15@gmail.com>
2020-09-17 21:17:06 -0300

    get product by id request

--- src/main/java/com/products/apirest/resources/ProductResource.java
+++ src/main/java/com/products/apirest/resources/ProductResource.java
@@ -4,6 +4,7 @@ import com.products.apirest.models.Product;
 import com.products.apirest.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,14 @@ public class ProductResource {
     @Autowired
     ProductRepository productRepository;
 
-    @GetMapping("/produtos")
-    public List<Product> listarProdutos() {
+    @GetMapping("/products")
+    public List<Product> listProducts() {
         return productRepository.findAll();
+    }
+
+    @GetMapping("/products/{id}")
+    public Product listarProdutById(@PathVariable(value = "id")  long id){
+        return productRepository.findById(id);
     }
 
 }
