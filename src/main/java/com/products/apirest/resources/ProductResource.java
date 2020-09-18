c5badcb00ab5675b54c426be2df276e349461cfe
Gilvan Henrique <gilvanh.15@gmail.com>
2020-09-18 10:24:52 -0300

    removing /products of mappings causing redundance

--- src/main/java/com/products/apirest/resources/ProductResource.java
+++ src/main/java/com/products/apirest/resources/ProductResource.java
@@ -8,22 +8,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/products")
 public class ProductResource {
     @Autowired
     ProductRepository productRepository;
 
-    @GetMapping("/products")
+    @GetMapping
     public List<Product> listProducts() {
         return productRepository.findAll();
     }
 
-    @GetMapping("/products/{id}")
-    public Product listarProdutById(@PathVariable(value = "id")  long id){
+    @GetMapping("/{id}")
+    public Product listProductById(@PathVariable(value = "id")  long id){
         return productRepository.findById(id);
     }
 
-    @PostMapping("/products")
+    @PostMapping
     public Product saveProduct(@RequestBody Product product){
         return productRepository.save(product);
     }
