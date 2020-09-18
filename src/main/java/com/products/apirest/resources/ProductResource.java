f0510c6683fa2a52ae7749efec67fe0f6eb11e2a
Gilvan Henrique <gilvanh.15@gmail.com>
2020-09-17 21:29:12 -0300

    post request save product

--- src/main/java/com/products/apirest/resources/ProductResource.java
+++ src/main/java/com/products/apirest/resources/ProductResource.java
@@ -3,10 +3,7 @@ package com.products.apirest.resources;
 import com.products.apirest.models.Product;
 import com.products.apirest.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +21,11 @@ public class ProductResource {
     @GetMapping("/products/{id}")
     public Product listarProdutById(@PathVariable(value = "id")  long id){
         return productRepository.findById(id);
+    }
+
+    @PostMapping("/products")
+    public Product saveProduct(@RequestBody Product product){
+        return productRepository.save(product);
     }
 
 }
