8f94030de1f3c5297415b0c145413610819fd33c
Gilvan Henrique <gilvanh.15@gmail.com>
2020-09-17 21:10:07 -0300

    get products request

--- src/main/java/com/products/apirest/resources/ProductResource.java
+++ src/main/java/com/products/apirest/resources/ProductResource.java
@@ -1,4 +1,23 @@
 package com.products.apirest.resources;
 
+import com.products.apirest.models.Product;
+import com.products.apirest.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
 public class ProductResource {
+    @Autowired
+    ProductRepository productRepository;
+
+    @GetMapping("/produtos")
+    public List<Product> listarProdutos() {
+        return productRepository.findAll();
+    }
+
 }
